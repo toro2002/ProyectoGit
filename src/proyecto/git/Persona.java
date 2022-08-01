006b7fd5392bee1c5243d3ce6395386ed0a2782d
IRIO ELIZONDO <IRIO ELIZONDO@10.50.41.243>
2022-08-01 14:58:09 -0600

    se agregan los metodos de acceso a la clase persona

--- src/proyecto/git/Persona.java
+++ src/proyecto/git/Persona.java
@@ -12,5 +12,30 @@ public class Persona {
     private int codigo;
     private String nombre;
     private String direccion;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
     
 }
